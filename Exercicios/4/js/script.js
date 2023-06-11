64a2b4dc40a70151c77236ab10f491526173fc29
Allan Santos <115902828+AllanSantos-DV@users.noreply.github.com>
2023-06-10 22:06:17 -0300

    Delete Exercicios directory

--- Exercicios/4/js/script.js
+++
@@ -1,9 +0,0 @@
-function calcularGasto() {
-  var quilometragem = parseFloat(document.getElementById("quilometragem").value);
-  var autonomia = parseFloat(document.getElementById("autonomia").value);
-  var valorCombustivel = parseFloat(document.getElementById("valorCombustivel").value);
-  var consumo = quilometragem / autonomia;
-  var gasto = consumo * valorCombustivel;
-
-  document.getElementById("resultado").innerHTML = "Gasto de R$" + gasto.toFixed(2) + " em Combustível";
-}
